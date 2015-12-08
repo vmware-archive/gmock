5ca503a2601fece9522b1e363634fe1b806d377d
Rafael Slobodian <rslobodian@pivotal.io>
2015-12-08 14:00:20 -0500

    RS - Refactored how panic is tested for better readability and to allow reuse.

--- gmock_test.go
+++ gmock_test.go
@@ -8,39 +8,72 @@ import (
 
 var _ = Describe("GMock", func() {
 	var subject *GMock
+	var constructSubject func()
+	var panicked bool
 
 	BeforeEach(func() {
 		subject = nil
+		constructSubject = nil
+		panicked = false
 	})
 
-	Describe("MockTarget", func() {
+	var panicRecover = func() {
+		panicked = recover() != nil
+	}
 
+	JustBeforeEach(func() {
+		defer panicRecover()
+		constructSubject()
+	})
+
+	var validMockTests = func(target interface{}) {
+		It("should not panic", func() {
+			Expect(panicked).To(BeFalse())
+		})
+
+		It("should return a valid GMock object", func() {
+			Expect(subject).NotTo(BeNil())
+		})
+
+		It("should have backed up the pointer to the original target", func() {
+			var originalPtr = subject.GetOriginal().Addr().Interface()
+			Expect(originalPtr).To(Equal(target))
+		})
+
+		It("should not have a mock value defined by default", func() {
+			var mockTargetPtr = subject.GetTarget().Addr().Interface()
+			Expect(mockTargetPtr).To(Equal(target))
+		})
+	}
+
+	Describe("MockTarget", func() {
 	    Context("when creating a new GMock with a target", func() {
-			var someVar = "some variable to mock"
+			someVar := "some variable to mock"
 
 			Context("and the target is not passed as a pointer", func() {
-				var callConstructorPassingValue = func() {
-					subject = CreateMockWithTarget(someVar)
-				}
+				BeforeEach(func() {
+					constructSubject = func() {
+						subject = CreateMockWithTarget(someVar)
+					}
+				})
 
 				It("should panic", func() {
-					Expect(callConstructorPassingValue).To(Panic())
+					Expect(panicked).To(BeTrue())
+				})
+
+				It("should not have constructed the mock", func() {
+				    Expect(subject).To(BeNil())
 				})
 			})
 
 			Context("and the target is passed as a pointer", func() {
-				var callConstructorPassingPointer = func() {
-					subject = CreateMockWithTarget(&someVar)
-				}
-
-				It("should not panic", func() {
-					Expect(callConstructorPassingPointer).NotTo(Panic())
+				BeforeEach(func() {
+					constructSubject = func() {
+						subject = CreateMockWithTarget(&someVar)
+					}
 				})
 
-				It("should return a valid GMock object", func() {
-					callConstructorPassingPointer()
-					Expect(subject).NotTo(BeNil())
-				})
+				validMockTests(&someVar)
 			})
 	    })
 	})
